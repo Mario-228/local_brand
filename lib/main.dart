171dacc9dd386f9654a09ffb33ef01321f8fe6cf
TDH9001 <mohamedyehia707@gmail.com>
2026-03-30 03:14:40 +0200

    the todo list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:local_brand/features/home_feature/presentation/home_feature_view.dart';
 
+//TODO: create a SettingsHiveBox > to add the current theme to the app
+//TODO: finish the fonts and colors in the core folder
+//TODO: create the navigaton serive using GoRouter
+
+
 void main() {
   runApp(const MyApp());
 }
