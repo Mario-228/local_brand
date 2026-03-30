a443d08ba8bf8e8f49f15b74c91b1b0bd6d95f44
TDH9001 <mohamedyehia707@gmail.com>
2026-03-30 03:41:19 +0200

    just updating some stuff

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:local_brand/features/home_feature/presentation/home_feature_view.dart';
 
 //TODO: create a SettingsHiveBox > to add the current theme to the app
-//TODO: finish the fonts and colors in the core folder
-//TODO: create the navigaton serive using GoRouter
-
+//TODO: finish the fonts in the core folder
+//TODO: create the navigation service using GoRouter
+//
 
 void main() {
   runApp(const MyApp());
@@ -16,11 +16,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Local Brand',
+      themeMode: ThemeMode.system, // Follows system theme (Light/Dark)
       theme: ThemeData(
-        colorScheme:ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const HomeFeatureView(),
     );
   }
 }
-
