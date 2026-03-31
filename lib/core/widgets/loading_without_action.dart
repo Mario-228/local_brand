4e6ea9ca9980be25c8d15fec32e55234a7e13169
Mario-228 <marioyoussef08@gmail.com>
2026-03-31 03:16:55 +0200

    create loading without action function

---
+++ lib/core/widgets/loading_without_action.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LoadingWithoutAction {
+  static void show(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return PopScope(
+          canPop: false,
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: const CircularProgressIndicator(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  static void hide(BuildContext context) {
+    Navigator.of(context, rootNavigator: true).pop();
+  }
+}
