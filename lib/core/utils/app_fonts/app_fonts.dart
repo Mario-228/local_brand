70cab89a7be08479f5fb09cc7313192888439d4a
TDH9001 <mohamedyehia707@gmail.com>
2026-03-31 03:05:56 +0200

    added fonts for loginScreen and for the Reusable TextFormFields

--- lib/core/utils/app_fonts/app_fonts.dart
+++ lib/core/utils/app_fonts/app_fonts.dart
@@ -1,8 +1,69 @@
 import 'package:flutter/material.dart';
 
 abstract class AppFonts {
-  static const TextStyle textStyleHeaderBold36 = TextStyle(
-    fontSize: 36,
+  static const TextStyle textStyleHeaderBold40 = TextStyle(
+    fontSize: 40,
+    fontFamily: "Inter",
     fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleTitleBold48 = TextStyle(
+    fontSize: 48,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleAppBarBold24 = TextStyle(
+    fontSize: 24,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleItemDescriptionTextRegular14 = TextStyle(
+    fontSize: 14,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleItemTitleSemiBold20 = TextStyle(
+    fontSize: 20,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleItemCostBold20 = TextStyle(
+    fontSize: 20,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleTotaLCheckoutCostBold36 = TextStyle(
+    fontSize: 36,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleLoginSideFontRegular16 = TextStyle(
+    fontSize: 16,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleLabelTextSemiBold12 = TextStyle(
+    fontSize: 12,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleTextFieldTextRegular16 = TextStyle(
+    fontSize: 16,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textButtonMixed14 = TextStyle(
+    fontSize: 14,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStylePrimaryButtonSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textGoogleLoginTextMedium14 = TextStyle(
+    fontSize: 14,
+    fontFamily: "Inter",
+    fontWeight: FontWeight.w500,
   );
 }
