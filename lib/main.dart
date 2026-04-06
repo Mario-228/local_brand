76b9e6f67b4d2025fe9de07a34aebbba5071ff1b
Mario-228 <marioyoussef08@gmail.com>
2026-04-06 15:58:31 +0200

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,13 @@ import 'package:local_brand/core/utils/routing_service/routing_service.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       routerConfig: RoutingService.routes,
       title: 'Local Brand',
       themeMode: ThemeMode.system, // Follows system theme (Light/Dark)
