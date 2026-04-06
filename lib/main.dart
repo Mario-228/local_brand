7f28c6a6d1a408facfe3bab2da76cc26fb3a74da
Mario-228 <marioyoussef08@gmail.com>
2026-04-06 13:08:06 +0200

    enable routing service in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:local_brand/core/utils/app_theme/app_theme.dart';
-import 'package:local_brand/features/home_feature/presentation/home_feature_view.dart';
+import 'package:local_brand/core/utils/routing_service/routing_service.dart';
 
 //TODO: create a SettingsHiveBox > to add the current theme to the app
 //TODO: finish the fonts in the core folder
@@ -14,12 +14,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: RoutingService.routes,
       title: 'Local Brand',
       themeMode: ThemeMode.system, // Follows system theme (Light/Dark)
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
-    home: const HomeFeatureView(),
     );
   }
 }
