659a31e3e1886617a3b439d8cf43cef86a277784
Mario-228 <marioyoussef08@gmail.com>
2026-04-06 16:02:17 +0200

    add signup view to the routes

--- lib/core/utils/routing_service/routing_service.dart
+++ lib/core/utils/routing_service/routing_service.dart
@@ -1,14 +1,21 @@
 import 'package:go_router/go_router.dart';
 import 'package:local_brand/features/home_feature/presentation/home_feature_view.dart';
 
+import '../../../features/signup_feature/presentation/signup_view.dart';
+
 abstract class RoutingService {
-  static final String kHomePage = '/';
+  static const String kHomePage = '/';
+  static const String kSignupPage = '/signup';
 
   static final routes = GoRouter(
     routes: <RouteBase>[
       GoRoute(
         path: kHomePage,
         builder: (context, state) => const HomeFeatureView(),
+      ),
+      GoRoute(
+        path: kSignupPage,
+        builder: (context, state) => const SignupView(),
       ),
     ],
   );
