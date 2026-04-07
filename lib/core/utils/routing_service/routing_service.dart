4dba3cacae201e22c801625f67611f541046dc85
TDH9001 <mohamedyehia707@gmail.com>
2026-04-07 02:03:13 +0200

    added login screen to routing service

--- lib/core/utils/routing_service/routing_service.dart
+++ lib/core/utils/routing_service/routing_service.dart
@@ -1,14 +1,20 @@
 import 'package:go_router/go_router.dart';
 import 'package:local_brand/features/home_feature/presentation/home_feature_view.dart';
+import 'package:local_brand/features/login_screen/presentation/login_screen.dart';
 
 abstract class RoutingService {
   static final String kHomePage = '/';
+  static final String kLoginScreen = 'loginScreen';
 
   static final routes = GoRouter(
     routes: <RouteBase>[
       GoRoute(
         path: kHomePage,
         builder: (context, state) => const HomeFeatureView(),
+      ),
+      GoRoute(
+        path: kLoginScreen,
+        builder: (context, state) => const LoginScreen(),
       ),
     ],
   );
