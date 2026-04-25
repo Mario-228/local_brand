6c2b0212862b3bb9a86476699da3181f269cbd95
TDH9001 <mohamedyehia707@gmail.com>
2026-04-25 16:57:01 +0300

    added inicilization to main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:local_brand/core/utils/app_theme/app_theme.dart';
 import 'package:local_brand/core/utils/routing_service/routing_service.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
 //TODO: create a SettingsHiveBox > to add the current theme to the app
-//TODO: finish the fonts in the core folder
-//TODO: create the navigation service using GoRouter
-//
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+
+  await Supabase.initialize(
+    url: 'https://czljfcnywirgnfphgwoq.supabase.co',
+    anonKey: 'sb_publishable_e5a-lRmCqZ4G6CrtS3eqWg_u8VBJSLD',
+  );
 }
 
 class MyApp extends StatelessWidget {
