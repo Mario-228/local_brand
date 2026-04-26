11f1a1971817e43414ddc0bf0e8c7ad512d1da66
Mario-228 <marioyoussef08@gmail.com>
2026-04-26 03:41:56 +0300

    add textStyleTextButtonSemiBold16

--- lib/core/utils/app_fonts/app_fonts.dart
+++ lib/core/utils/app_fonts/app_fonts.dart
@@ -129,4 +129,12 @@ abstract class AppFonts {
         fontWeight: FontWeight.w500,
         color: getColorObject(context).onSurface,
       );
+
+  static TextStyle textStyleTextButtonSemiBold16(BuildContext context) =>
+      TextStyle(
+        fontSize: 16,
+        fontFamily: "Inter",
+        fontWeight: FontWeight.w600,
+        color: getColorObject(context).surface,
+      );
 }
