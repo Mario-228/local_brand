38a7020dca1b43c49624fdd24e5b928d41822002
Mario-228 <marioyoussef08@gmail.com>
2026-04-26 03:43:03 +0300

    edit CustomMaterialButton widget

--- lib/core/widgets/custom_material_button.dart
+++ lib/core/widgets/custom_material_button.dart
@@ -7,21 +7,32 @@ class CustomMaterialButton extends StatelessWidget {
     required this.text,
     this.onPressed,
     this.height = 50.0,
+    this.color,
+    this.textStyle,
+    this.extraWidget,
   });
   final String text;
   final void Function()? onPressed;
   final double height;
+  final Color? color;
+  final TextStyle? textStyle;
+  final Widget? extraWidget;
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
       minWidth: double.infinity,
       onPressed: onPressed,
-      color: Theme.of(context).extension<AppThemeColors>()!.colors.primary,
+      color:
+          color ??
+          Theme.of(context).extension<AppThemeColors>()!.colors.primary,
       height: height,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.0)),
-      child: Text(
-        text,
-        // style: AppFonts.textStyleBold19,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ?extraWidget,
+          Text(text, style: textStyle),
+        ],
       ),
     );
   }
