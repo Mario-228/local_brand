81177af0769d15d16cd2b6509a5a8f4694212b76
Mario-228 <marioyoussef08@gmail.com>
2026-06-03 05:56:28 +0300

    add alignment parameter to make the icon in the topCenter and isTablet parameter

--- lib/features/forget_password_feature/presentation/widgets/forget_lock_icon.dart
+++ lib/features/forget_password_feature/presentation/widgets/forget_lock_icon.dart
@@ -2,17 +2,32 @@ import 'package:flutter/material.dart';
 import '../../../../core/utils/app_fonts/app_fonts.dart';
 
 class ForgetLockIcon extends StatelessWidget {
-  const ForgetLockIcon({super.key, required this.lockIcon});
+  const ForgetLockIcon({
+    super.key,
+    required this.lockIcon,
+    required this.alignment,
+    required this.isTablet,
+  });
   final IconData lockIcon;
+  final Alignment alignment;
+  final bool isTablet;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: AppFonts.getColorObject(context).backgroundVariantColorMain,
-      elevation: 0,
-      child: Icon(
-        lockIcon,
-        size: 65,
-        color: AppFonts.getColorObject(context).primary,
+    return Align(
+      alignment: alignment,
+      child: Card(
+        color: isTablet
+            ? null
+            : AppFonts.getColorObject(context).backgroundVariantColorMain,
+        elevation: 0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(
+            lockIcon,
+            size: 65,
+            color: AppFonts.getColorObject(context).primary,
+          ),
+        ),
       ),
     );
   }
