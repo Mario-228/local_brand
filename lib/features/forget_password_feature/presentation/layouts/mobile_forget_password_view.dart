bade62166038cb13dddf877212dab092cddeb100
Mario-228 <marioyoussef08@gmail.com>
2026-06-03 05:57:15 +0300

    edit mobile forget password view to use it in tablet view

--- lib/features/forget_password_feature/presentation/layouts/mobile_forget_password_view.dart
+++ lib/features/forget_password_feature/presentation/layouts/mobile_forget_password_view.dart
@@ -5,18 +5,25 @@ import '../widgets/forget_password_texts.dart';
 import '../widgets/return_to_login_text_button.dart';
 
 class MobileForgetPasswordView extends StatelessWidget {
-  const MobileForgetPasswordView({super.key});
-
+  const MobileForgetPasswordView({super.key, required this.isScrollable});
+  final bool isScrollable;
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
+      physics: isScrollable
+          ? const BouncingScrollPhysics()
+          : const NeverScrollableScrollPhysics(),
       child: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           spacing: 20.0,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ForgetLockIcon(lockIcon: Icons.lock_reset),
+            ForgetLockIcon(
+              lockIcon: isScrollable ? Icons.lock_reset : Icons.key_outlined,
+              alignment: isScrollable ? Alignment.topLeft : Alignment.center,
+              isTablet: !isScrollable,
+            ),
             ForgetPasswordTexts(),
             EmailFieldAndResetButton(),
             ReturnToLoginTextButton(),
