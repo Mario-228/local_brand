b02251d115acff9a5a022757665a33128f77d157
Mario-228 <marioyoussef08@gmail.com>
2026-06-03 05:20:05 +0300

    add messengerRightArrow to constants

--- lib/core/utils/constants/constants.dart
+++ lib/core/utils/constants/constants.dart
@@ -3,4 +3,5 @@ abstract class Constants {
   static const String upArrowCode = '\u2191';
   static const String rightArrowCode = '\u2192';
   static const String downArrowCode = '\u2193';
+  static const String messengerRightArrow = '\u27A3';
 }
