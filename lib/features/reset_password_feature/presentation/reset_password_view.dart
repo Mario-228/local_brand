d9d1d932c8b7b34c5ee0e0ef8b1b38cd312f9611
Mario-228 <marioyoussef08@gmail.com>
2026-07-01 05:02:39 +0300

    create tablet reset view

--- lib/features/reset_password_feature/presentation/reset_password_view.dart
+++ lib/features/reset_password_feature/presentation/reset_password_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:local_brand/core/utils/responsive_service/responsive_service.dart';
 import 'package:local_brand/core/widgets/custom_appbar.dart';
 import 'package:local_brand/features/reset_password_feature/presentation/layouts/mobile_reset_view.dart';
+import 'package:local_brand/features/reset_password_feature/presentation/layouts/tablet_reset_view.dart';
 
 class ResetPasswordView extends StatelessWidget {
   const ResetPasswordView({super.key});
@@ -10,7 +11,10 @@ class ResetPasswordView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppbar(title: 'Reset Password'),
-      body: ResponsiveService(mobile: (context) => const MobileResetView()),
+      body: ResponsiveService(
+        mobile: (context) => const MobileResetView(),
+        tablet: (context) => const TabletResetView(),
+      ),
     );
   }
 }
